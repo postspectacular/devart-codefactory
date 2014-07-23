a19817e68a355227311bbee4f919941e7654a360
Karsten Schmidt <k@postspectacular.com>
2014-07-23 13:51:22 +0100

    memoize color fns

--- src-async/codefactory/color.cljs
+++ src-async/codefactory/color.cljs
@@ -6,26 +6,28 @@
    [thi.ng.geom.core.vector :as v :refer [vec3]]
    [thi.ng.common.math.core :as m]))
 
-(defn hex->rgb
-  [hex]
-  (let [h (js/parseInt (subs hex 1) 16)
-        i (/ 255.0)]
-    [(* (bit-and (bit-shift-right h 16) 0xff) i)
-     (* (bit-and (bit-shift-right h 8) 0xff) i)
-     (* (bit-and h 0xff) i)]))
+(def hex->rgb
+  (memoize
+   (fn [hex]
+     (let [h (js/parseInt (subs hex 1) 16)
+           i (/ 255.0)]
+       [(* (bit-and (bit-shift-right h 16) 0xff) i)
+        (* (bit-and (bit-shift-right h 8) 0xff) i)
+        (* (bit-and h 0xff) i)]))))
 
-(defn rgb->hex
-  [r g b]
-  (let [c (bit-or
-           (bit-shift-left (int (* r 255)) 16)
-           (bit-shift-left (int (* g 255)) 8)
-           (int (* b 255)))
-        hex (.toString (js/Number. c) 16)
-        len (count hex)]
-    (str "#"
-         (if (< len 6)
-           (str (subs "00000" 0 (- 6 len)) hex)
-           hex))))
+(def rgb->hex
+  (memoize
+   (fn [r g b]
+     (let [c (bit-or
+              (bit-shift-left (int (* r 255)) 16)
+              (bit-shift-left (int (* g 255)) 8)
+              (int (* b 255)))
+           hex (.toString (js/Number. c) 16)
+           len (count hex)]
+       (str "#"
+            (if (< len 6)
+              (str (subs "00000" 0 (- 6 len)) hex)
+              hex))))))
 
 (defn gray-offset-hex
   [hex x]
