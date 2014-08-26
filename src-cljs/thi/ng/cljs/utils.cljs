8397fd77a08e1152b8d855e23b38b0a4057d75cd
Karsten Schmidt <k@postspectacular.com>
2014-08-26 15:29:11 +0100

    add html-entities, as-str & escape-html helpers

--- src-cljs/thi/ng/cljs/utils.cljs
+++ src-cljs/thi/ng/cljs/utils.cljs
@@ -1,6 +1,21 @@
 (ns thi.ng.cljs.utils
   (:require
-   [goog.string.format]))
+   [goog.string.format]
+   [clojure.string :as str]))
+
+(def html-entities
+  {\& "&amp;"
+   \< "&lt;"
+   \> "&gt;"
+   \" "&quot;"})
+
+(defn as-str [x]
+  (if (or (keyword? x) (symbol? x))
+    (name x)
+    (str x)))
+
+(defn escape-html
+  [x] (str/escape (as-str x) html-entities))
 
 (defn deep-merge-with
   "Like merge-with, but merges maps recursively, applying the given fn
